6eec3b7e048523e74703ede39deeb42e5b2c8358
jaime <jaumecloquell@hotmail.com>
2019-02-13 00:37:51 +0100

    .

--- decisionTree/preprocess/selectorVariables.R
+++ decisionTree/preprocess/selectorVariables.R
@@ -2,10 +2,25 @@ library(mlbench)
 library(FSelector)
 
 selector.random.forest <- function (formula, data, importance.type = 1) {
-  weights <- FSelector::random.forest.importance(formula, data, importance.type=importance.type)
+  weights <- FSelector::random.forest.importance(C~., data, importance.type=importance.type)
   # se muestran los resultados
   print(weights)
-  subset <- cutoff.k(weights,5)
+  subset <- cutoff.k(weights,dim(data)[2])
   f <- as.simple.formula(subset,"Class")
   print(f)
+}
+
+
+selector.chiSquare <- function (data) {
+  weights <- FSelector::chi.squared(C~.,data)
+  print(weights)
+  
+  # se seleccionan los 5 mejores
+  subset <- FSelector::cutoff.k(weights,dim(data)[2])
+  
+  # se muestran los seleccionados
+  f <- as.simple.formula(subset,"Class")
+  print(f)
+  
+  
 }
